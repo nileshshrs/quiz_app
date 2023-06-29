6bab33f2a79614fdbefe7b7bf9de49d960041ced
nileshshrs <nileshshr54@gmail.com>
2023-06-29 15:13:54 +0545

    removed unecessary test files that should not be commited

--- src/main/java/com/quiz/View/TeacherQuizPage.java
+++ src/main/java/com/quiz/View/TeacherQuizPage.java
@@ -22,7 +22,7 @@ import javax.swing.WindowConstants;
 
 import com.quiz.Model.Profile;
 import com.quiz.Model.QuizQuestionModel;
-import com.quiz.Model.UserScore;
+
 import com.quiz.View.Theme.Sidebar;
 
 public class TeacherQuizPage extends JFrame {
@@ -32,7 +32,7 @@ public class TeacherQuizPage extends JFrame {
     private String USERNAME;
     private int id;
     private Profile profileData;
-    private UserScore userScore;
+
 
     public TeacherQuizPage(String username, int ID) {
         this.id = ID;
@@ -111,10 +111,7 @@ public class TeacherQuizPage extends JFrame {
                 } else if (buttonText.equals("Take Quiz")) {
                     currentPanel = quiz;
                     contentPanel.add(currentPanel);
-                } else if (buttonText.equals("Your Scores")) {
-                    setScorePanel(id);
-
-                } else if (buttonText.equals("Log Out")) {
+                }  else if (buttonText.equals("Log Out")) {
                     disposeWindow();
                     return;
                 } else {
@@ -140,13 +137,7 @@ public class TeacherQuizPage extends JFrame {
         setVisible(true);
     }
 
-    private void setScorePanel(int id) {
-        this.userScore = new UserScore(id);
-        ArrayList<String[]> scoreData = userScore.getUserScore();
 
-        currentPanel = new ScoreView(scoreData);
-        contentPanel.add(currentPanel);
-    }
 
     // logout
     private void disposeWindow() {
